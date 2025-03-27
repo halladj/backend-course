e8c660f20907b6b00f5c51aff286dc84e8337c52
halladj <halladj00@gmail.com>
2025-03-27 12:25:13 +0100

    bad manual validation

---
+++ src/session-6/index.js
@@ -0,0 +1,69 @@
+import express from "express";
+import {z} from "zod";
+
+let cart = [
+    {id: 1, name:"Apple", quantity: 12, price:400},
+    {id: 2, name:"Chocolate", quantity: 1, price:250}
+]
+
+const cartItemSchema = z.object({
+    id: z.number().int(),
+    name: z.string().max(20),
+    quantity: z.number().int().lt(100),
+    price: z.number().gt(0)
+});
+
+// 1. ceate new express application(object).
+const app = express()
+
+app.use(express.json())
+
+// 2. define index route.
+app.get("/cart", (req, res) => {
+    res.send(cart)
+});
+
+app.post("/cart", (req, res) => {
+    if(!req.body.id){
+        res.send("Bad Request: did not include 'id'.")
+    }
+    if(!req.body.name){
+        res.send("Bad Request: did not include 'name'.")
+    }
+
+    if(!req.body.quantity){
+        res.send("Bad Request: did not include 'quantity'.")
+    }
+    if(!req.body.price){
+        res.send("Bad Request: did not include 'price'.")
+    }
+    const data = req.body;
+    cart.push(data)
+    res.status(201)
+    res.send(data)
+});
+
+app.delete("/cart/:id",(req, res) => {
+    cart = cart.filter((value) => {
+        return value.id != req.params.id;
+    });
+
+    console.log(cart)
+    res.send(cart)
+})
+
+app.put("/cart/:id", (req, res) => {
+    const item = cart.filter( (value)=> {
+        return value.id == req.params.id
+    } );
+
+    if (item != []){
+        item[0].name= req.body.name;
+    }
+
+    res.send(cart)
+});
+
+app.listen(3000,() => {
+    console.log("App is running on port:3000 ...")
+})
